40b8060a06882c8bce3f0d30762cccb3c83f0ce7
dlscoccia <dlscoccia@gmail.com>
2021-08-27 13:05:37 -0500

    Adding new challenges

--- 50_retos/13_Live_User_Filter/script.js
+++ 50_retos/13_Live_User_Filter/script.js
@@ -9,27 +9,29 @@ filter.addEventListener('input', (e) => filterData(e.target.value))
 async function getData() {
     const res = await fetch('https://randomuser.me/api?results=50')
     const data = await res.json()
-   // const {  } = json()
+    const info = data.results.map(info => ([info.name, info.location, info.picture]))
 
-    // Clear result
     result.innerHTML = ''
 
-    (user => {
+    info.map(user => {
         const li = document.createElement('li')
 
         listItems.push(li)
 
         li.innerHTML = `
- 
+            <img src='${user[2].thumbnail}'/>
+            <div class="user-info">    
+            <h4>${user[0].first} ${user[0].last}</h4>
+            <p>${user[1].city}, ${user[1].country}</p>
+            </div>
         `
-
         result.appendChild(li)
     })
 }
 
 function filterData(searchTerm) {
     listItems.forEach(item => {
-        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+        if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
             item.classList.remove('hide')
         } else {
             item.classList.add('hide')
