d4ec7d78ee632dff3c271a72fe42c5d0634e1114
dlscoccia <dlscoccia@gmail.com>
2021-08-28 20:14:57 -0500

    updating grid component

--- React/pokedex_api/src/components/PokeGrid.js
+++ React/pokedex_api/src/components/PokeGrid.js
@@ -66,15 +66,29 @@ const PokeGrid = () => {
             <form onSubmit={handleSubmit} className={style.form}>
                 <label className={style.label}>
                     Filtrar por tipo:
-                    <select onChange={handleChangeType}>
+                    <select onChange={handleChangeType} className={style.select}>
                     <option value=""></option>
                         <option value="water">Water</option>
                         <option value="fire">Fire</option>
                         <option value="grass">Grass</option>
                         <option value="normal">Normal</option>
+                        <option value="electric">Electric</option>
+                        <option value="dark">Dark</option>
+                        <option value="ice">Ice</option>
+                        <option value="bug">Bug</option>
+                        <option value="ground">Ground</option>
+                        <option value="dragon">Dragon</option>
+                        <option value="steel">Steel</option>
+                        <option value="rock">Rock</option>
+                        <option value="poison">Poison</option>
+                        <option value="psychic">Psychic</option>
+                        <option value="ghost">Ghost</option>
+                        <option value="fairy">Fairy</option>
+                        <option value="flying">Flying</option>
+                        <option value="fighting">Fighting</option>
                     </select>
                 </label>
-                <input type="submit" value="Buscar" onSubmit={handleSubmit}/>
+                <button type="button" className={style.btn} onClick={handleSubmit}>Filtrar</button>
             </form>
             <ul className={style.pokeList}>
                 {
